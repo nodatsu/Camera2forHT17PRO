d21e47a56ea1dda10d88e2745fc5856dee814390
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2017-06-16 17:14:38 +0900

    とりあえず動く

---
+++ README.md
@@ -0,0 +1,8 @@
+# HT17PROのセンサ機能確認用
+
+(参考) AndroidのCamera2 APIとOpenGL ESでカメラの映像を表示してみた
+http://qiita.com/ueder/items/16be80bd1fc9ac8b0c1a
+
+* Minimum SDK 6.0で作成
+* アプリ設定でカメラを許可する必要がある
+
---
+++ app/src/main/java/com/example/tsunoda/camera2forht17pro/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.tsunoda.camera2forht17pro;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Toast;
+
+import java.util.Collections;
+
+public class MainActivity extends Activity {
+    private Camera mCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextureView textureView = (TextureView) findViewById(R.id.texture_view);
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                mCamera.open();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {}
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {}
+        });
+
+        mCamera = new Camera(textureView);
+    }
+
+    class Camera {
+        private CameraDevice mCamera;
+        private TextureView mTextureView;
+        private Size mCameraSize;
+        private CaptureRequest.Builder mPreviewBuilder;
+        private CameraCaptureSession mPreviewSession;
+
+        private CameraDevice.StateCallback mCameraDeviceCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                mCamera = camera;
+                createCaptureSession();
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+                camera.close();
+                mCamera = null;
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+                camera.close();
+                mCamera = null;
+            }
+        };
+
+        CameraCaptureSession.StateCallback mCameraCaptureSessionCallback = new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession session) {
+                mPreviewSession = session;
+                updatePreview();
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                Toast.makeText(MainActivity.this, "onConfigureFailed", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        public Camera(TextureView textureView) {
+            mTextureView = textureView;
+        }
+
+        public void open() {
+            try {
+                CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                for (String cameraId : manager.getCameraIdList()) {
+                    CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                    if (characteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
+                        StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                        mCameraSize = map.getOutputSizes(SurfaceTexture.class)[0];
+                        manager.openCamera(cameraId, mCameraDeviceCallback, null);
+
+                        return;
+                    }
+                }
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void createCaptureSession() {
+            if (!mTextureView.isAvailable()) {
+                return;
+            }
+
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(mCameraSize.getWidth(), mCameraSize.getHeight());
+            Surface surface = new Surface(texture);
+            try {
+                mPreviewBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+            mPreviewBuilder.addTarget(surface);
+            try {
+                mCamera.createCaptureSession(Collections.singletonList(surface), mCameraCaptureSessionCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void updatePreview() {
+            mPreviewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            HandlerThread thread = new HandlerThread("CameraPreview");
+            thread.start();
+            Handler backgroundHandler = new Handler(thread.getLooper());
+
+            try {
+                mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, backgroundHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
